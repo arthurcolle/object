ba8bfc3ca42f2c50bfddafa2a0f65491c07ebb5a
Arthur Colle <arthur@collegroup.com>
2025-06-12 03:27:07 -0400

    object

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Object.MixProject do
   def project do
     [
       app: :object,
-      version: "0.1.0",
+      version: "0.1.2",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
